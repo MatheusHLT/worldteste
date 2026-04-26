f222103f8f3244b6829bffad8510020f5600bebc
MatheusHLT <matheus.honorato.lt@gmail.com>
2026-04-26 18:10:38 -0300

    Add novo mentário e mudança de Olá -> Oi

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+// Este é um arquivo onde eu irei testar e aplicar conhecimentos aprendidos de uma forma livre
+
 import java.util.Scanner;
 
 public class App {
 
     public static void hello(String name) {
-        System.out.println("Olá, " + name);
+        System.out.println("Oi, " + name);
     }
     public static void main(String[] args) throws Exception {
         Scanner typeName = new Scanner(System.in);
@@ -15,7 +17,6 @@ public class App {
         int a = 0;
 
         while (a < 10) {
-
             if (a == 10) {
                 a = 0;
             }
